41ff6d43fdc4c633a8e715ad64a5cc68ae58a2ad
Risabhs <Risabhs@corp.rpqb.com>
2017-05-02 11:59:30 +0530

    read1 added

--- UserTest.go
+++ UserTest.go
@@ -100,6 +100,9 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	if function == "read" { //read a variable
 		return t.read(stub, args)
 	}
+	if function == "read1" { //read a variable
+		return t.read1(stub, args)
+	}
 	fmt.Println("query did not find func: " + function)
 
 	return nil, errors.New("Received unknown function query: " + function)
@@ -130,6 +133,25 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	var err error
 
 	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
+// read1 - query function to read key/value pair
+func (t *SimpleChaincode) read1(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 3 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
